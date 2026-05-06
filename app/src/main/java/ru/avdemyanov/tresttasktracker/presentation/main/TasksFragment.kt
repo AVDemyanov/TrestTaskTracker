2e14966e9f0fef5323346601ce89b417801f6477
Андрей <avdemyanov@icloud.com>
2026-05-06 12:46:27 +0500

    Add Crashlytics v1

--- app/src/main/java/ru/avdemyanov/tresttasktracker/presentation/main/TasksFragment.kt
+++ app/src/main/java/ru/avdemyanov/tresttasktracker/presentation/main/TasksFragment.kt
@@ -40,6 +40,7 @@ class TasksFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         val app = requireActivity().application as TaskTrackerApplication
         val factory = ViewModelFactory(
             mapOf(
